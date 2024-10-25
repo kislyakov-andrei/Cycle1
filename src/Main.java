07114e4747d63309edfd4a12a40599be1cd71b89
kislyakov-andrei <kisli@mail.ru>
2024-10-26 00:29:15 +0300

    ad hw

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,14 @@ public class Main {
         for (int i=0; i<=17; i+=2) {
             System.out.println(i);
         }
+        for (int i=1; i<=14; i++){
+            System.out.println(i*7);}
+        for (int i=1; i<=512; i=i*2){
+            System.out.println(i);}
 
-        for (int i=1; i<=10; i+=1){
-            int a=i*2;
-            System.out.println("2 x "+i+ " = " + a);}
+        //for (int i=1; i<=10; i+=1){
+        //    int a=i*2;
+         //   System.out.println("2 x "+i+ " = " + a);}
 
         }
     }
