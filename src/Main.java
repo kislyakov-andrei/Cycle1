9a02a428864d4b81915c96f33ea167d79a1b65c3
kislyakov-andrei <kisli@mail.ru>
2024-10-26 00:13:52 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         for (int i=0; i<=10; i+=1) {
-            System.out.printf(i+", ");
-           // System.out.println(i);
+            //System.out.printf(i+", ");
+            System.out.println(i);
         }
+        for (int i=10; i>0; i--)
+        {
+            System.out.println(i);}
+        for (int i=0; i<=17; i+=2) {
+            System.out.println(i);
+        }
+
         for (int i=1; i<=10; i+=1){
             int a=i*2;
             System.out.println("2 x "+i+ " = " + a);}
