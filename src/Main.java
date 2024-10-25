7a31ada63114e027e64cfe618786c309c7260a95
kislyakov-andrei <kisli@mail.ru>
2024-10-25 23:53:05 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i=0; i<=10; i+=1) {
+            System.out.printf(i+", ");
+           // System.out.println(i);
+        }
+        for (int i=1; i<=10; i+=1){
+            int a=i*2;
+            System.out.println("2 x "+i+ " = " + a);}
+
+        }
+    }
