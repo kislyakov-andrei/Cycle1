e35ebda42b6eaa0ba532a170a22e128a43839b12
kislyakov-andrei <kisli@mail.ru>
2024-10-26 19:17:28 +0300

    ad hw

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,59 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i=0; i<=10; i+=1) {
+        // Task 1
+        System.out.println("Task 1");
+        for (int i=1; i<=10; i+=1) {
             //System.out.printf(i+", ");
             System.out.println(i);
         }
+        // Task 2
+        System.out.println("Task 2");
         for (int i=10; i>0; i--)
         {
             System.out.println(i);}
-        for (int i=0; i<=17; i+=2) {
+        // Task 3
+        System.out.println("Task 3");
+        for (int i=2; i<=17; i+=2) {
             System.out.println(i);
         }
+        //Task 4
+        System.out.println("Task 4");
+        for (int i=10; i>=-10; i--) {
+            System.out.println(i);}
+        // Task 5
+        System.out.println("Task 5");
+        for (int i=1904; i<=2096; i+=4) {
+            if (i %4 == 0 && i % 100 != 0) {
+                System.out.println( i+ " год является високосным");}}
+
+        // Task 6
+        System.out.println("Task 6");
         for (int i=1; i<=14; i++){
             System.out.println(i*7);}
-        for (int i=1; i<=512; i=i*2){
+        // or
+        for (int i=7; i<=98; i+=7){
             System.out.println(i);}
 
-        //for (int i=1; i<=10; i+=1){
-        //    int a=i*2;
-         //   System.out.println("2 x "+i+ " = " + a);}
+        // Task 7
+        System.out.println("Task 7");
+        for (int i=1; i<=512; i=i*2){
+            System.out.println(i);}
+        // Task 8
+        System.out.println("Task 8");
+        int salary=29000;
+        int total=0;
+        for (int i=0; i<=12; i++) {
+            total=total+salary;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей.");
+        }
+        // Task 9
+        System.out.println("Task 9");
+
+        // Task 10
+        System.out.println("Task 10");
+        for (int i=1; i<=10; i+=1){
+            int a=i*2;
+            System.out.println("2 x "+i+ " = " + a);}
 
         }
     }
